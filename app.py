707787abff6f5f892ddc789158dbd75a08ac2ed2
Kundan <kundanims2020@gmail.com>
2024-07-25 20:12:27 +0530

    added CORS

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 import index_service  # Import the service module
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/query_index', methods=['POST'])
 def query_index():
